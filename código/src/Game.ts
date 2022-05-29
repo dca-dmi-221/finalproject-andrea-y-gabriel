3213bf4d30f3f3d169813a310956510f76d0f523
Andrea Torrente <andrea.torrente1@u.icesi.edu.co>
2022-05-28 19:17:31 -0500

    avance movimiento

--- código/src/Game.ts
+++ código/src/Game.ts
@@ -1,18 +1,61 @@
 /* eslint-disable no-param-reassign */
 import './style.css';
 import p5 from 'p5';
+import Player from './Player';
+import Map from './Map';
+
+const LEVEL1 = new Map();
+const PLAYER1 = new Player(1, 1);
+const PLAYER2 = new Player(17, 1);
 
 const sketch = (p: p5) => {
   p.setup = () => {
-    p.createCanvas(1200, 500);
+    p.createCanvas(1200, 720);
+    PLAYER1.setMap(LEVEL1);
+    PLAYER2.setMap(LEVEL1);
   };
 
   p.draw = () => {
     p.background(10);
+    LEVEL1.show(p);
+    PLAYER1.show(p);
+    PLAYER2.show(p);
   };
 
   p.keyPressed = () => {
+    const K = p.key.toLowerCase();
 
+    if (p.keyCode === p.RIGHT_ARROW) {
+      PLAYER1.move('RIGHT');
+    }
+
+    if (p.keyCode === p.LEFT_ARROW) {
+      PLAYER1.move('LEFT');
+    }
+
+    if (p.keyCode === p.UP_ARROW) {
+      PLAYER1.move('UP');
+    }
+
+    if (p.keyCode === p.DOWN_ARROW) {
+      PLAYER1.move('DOWN');
+    }
+
+    if (K === 'd') {
+      PLAYER2.move('RIGHT');
+    }
+
+    if (K === 'a') {
+      PLAYER2.move('LEFT');
+    }
+
+    if (K === 'w') {
+      PLAYER2.move('UP');
+    }
+
+    if (K === 's') {
+      PLAYER2.move('DOWN');
+    }
   };
 };
 // eslint-disable-next-line new-cap
--- código/src/Player.ts
+++ código/src/Player.ts
@@ -1,18 +1,87 @@
 import p5 from 'p5';
+import Map from './Map';
+import { PlayerDirection } from './Type';
+
+const PLAYERSIZE = 48;
 
 export default class Player {
   posX : number;
   posY: number;
-  b: number;
+  pcFil: number;
+  pcCol: number;
+  image1: any;
+  image2: any;
+  image3: any;
+  image4: any;
+  direction: string;
 
-  constructor() {
-    this.posX = 175;
-    this.posY = 175;
-    this.b = 25;
+  refMap: Map | null = null;
+
+  constructor(fil: number, col: number) {
+    this.pcFil = fil;
+    this.pcCol = col;
+    this.direction = 'DOWN';
+    this.posX = (fil * PLAYERSIZE) + 288; // + 312;
+    this.posY = (col * PLAYERSIZE);
+    this.refMap = null;
+    // this.image1 = p.loadImage('../assests/Player/player1A.png');
+    // this.image2 = p.loadImage('../assests/Player/playerB.png');
+    // this.image3 = p.loadImage('../assests/Player/player1C.png');
+    // this.image4 = p.loadImage('../assests/Player/player1D.png');
   }
 
-  show(p:p5) {
-    p.fill(10, 180, 10);
-    p.rect(this.posX, this.posY, this.b, this.b);
+  show(p: p5) {
+    p.fill(180, 10, 200);
+    p.rect(this.posX, this.posY, PLAYERSIZE);
+    p.rectMode(p.CORNER);
+    // p.imageMode(p.CENTER);
+    // if (this.d === 0) {
+    //   p.image(this.image1, this.posX, this.posY);
+    // } else if (this.d === 1) {
+    //   p.image(this.image2, this.posX, this.posY);
+    // } else if (this.d === 2) {
+    //   p.image(this.image3, this.posX, this.posY);
+    // } else {
+    //   p.image(this.image4, this.posX, this.posY);
+    // }
+    // p.imageMode(p.CORNER);
+  }
+
+  move(direction: PlayerDirection) {
+    switch (direction) {
+      case 'RIGHT':
+        if (this.refMap?.canMove(this.pcFil, this.pcCol + 1)) {
+          this.pcCol += 1;
+        }
+        break;
+      case 'LEFT':
+        if (this.refMap?.canMove(this.pcFil, this.pcCol - 1)) {
+          this.pcCol -= 1;
+        }
+        break;
+      case 'UP':
+        if (this.refMap?.canMove(this.pcFil - 1, this.pcCol)) {
+          this.pcFil -= 1;
+        }
+        break;
+      case 'DOWN':
+        if (this.refMap?.canMove(this.pcFil + 1, this.pcCol)) {
+          this.pcFil += 1;
+        }
+        break;
+      default:
+        break;
+    }
+    this.direction = direction;
+    this.truePosition();
+  }
+
+  truePosition() {
+    this.posX = (this.pcCol * PLAYERSIZE) + 288;
+    this.posY = (this.pcFil * PLAYERSIZE);
+  }
+
+  setMap(m:Map) {
+    this.refMap = m;
   }
 }
